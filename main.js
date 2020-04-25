9baf2cb428d5ba6c33864df80caddcfa8c1c6c9c
roblatacz <roblatacz@gmail.com>
2020-04-25 21:44:15 +0100

    started adding basic css but still needs work

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const textBox = document.querySelector("#todoText");
 const ul = document.querySelector("#list");
 
 // get the value of the todo text when clicking Add
-
-document.getElementById("btn").addEventListener("click", function () {
+document.getElementById("add-btn").addEventListener("click", function () {
   const li = document.createElement("li");
   li.innerHTML = textBox.value;
   ul.appendChild(li);
+  document.getElementById("todoText").value = "";
 });
 
+//remove last li in list
 document.getElementById("remove-btn").addEventListener("click", function () {
   ul.lastElementChild.remove();
 });
