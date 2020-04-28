404ddc185241f8a72cd5ebe4dc4426ba80d3dce8
roblatacz <roblatacz@gmail.com>
2020-04-28 19:57:38 +0100

    Added functionality to delete a task however this needs improvements. The delete button fuction needs to be able to run outside of the add task function. Also need to be able to properly delete just the one line of tasks rather then all tasks

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const textBox = document.querySelector("#todoText");
 const ul = document.querySelector("#list");
 const listItem = document.querySelectorAll(".listItem");
-const lilength = document.querySelectorAll(".li");
 
 // get the value of the todo text when clicking Add
 document.getElementById("add-btn").addEventListener("click", function () {
   const li = document.createElement("li");
   const libtn = document.createElement("button");
+  const lilength = document.querySelectorAll(".li");
   libtn.innerHTML = "Delete";
   libtn.setAttribute("id", "del");
   li.setAttribute("class", "li");
@@ -14,13 +14,23 @@ document.getElementById("add-btn").addEventListener("click", function () {
   ul.appendChild(li);
   document.getElementById("todoText").value = "";
   li.appendChild(libtn);
-  const lilength = document.querySelectorAll(".li");
   console.log(lilength.length);
-});
+  deleteItem();
 
-// if (lilength.length > 0) {
-//   console.log("hello");
-// }
+  function deleteItem() {
+    if (lilength.length > 0) {
+      console.log("atleast 1 li");
+    } else {
+      console.log("0 li");
+    }
+  }
+
+  //delete line if delete button is clicked
+  document.getElementById("del").addEventListener("click", function () {
+    const del = document.querySelector("#del");
+    del.parentNode.remove();
+  });
+});
 
 //remove last li in list
 // document.getElementById("remove-btn").addEventListener("click", function () {
