eb521365e085f3322429239c4636b8c1c0dde005
roblatacz <roblatacz@gmail.com>
2020-05-06 21:28:12 +0100

    Starting main.js again with neater function struture

--- main.js
+++ main.js
@@ -1,49 +1,12 @@
-const textBox = document.querySelector("#todoText");
-const ul = document.querySelector("#list");
-const listItem = document.querySelectorAll(".listItem");
-
-// get the value of the todo text when clicking Add
-document.getElementById("add-btn").addEventListener("click", function () {
+// function to create the task li
+function createTask() {
   const li = document.createElement("li");
-  const libtn = document.createElement("button");
-  const lilength = document.querySelectorAll(".li");
-  libtn.innerHTML = "Delete";
-  libtn.setAttribute("id", "del");
-  li.setAttribute("class", "li");
-  li.innerHTML = textBox.value;
+  const input = document.getElementById("input-id");
+  const ul = document.getElementById("ul-id");
+  li.innerHTML = input.value;
   ul.appendChild(li);
-  document.getElementById("todoText").value = "";
-  li.appendChild(libtn);
-  console.log(lilength.length);
-  deleteItem();
+  console.log(ul);
+}
 
-  function deleteItem() {
-    if (lilength.length > 0) {
-      console.log("atleast 1 li");
-    } else {
-      console.log("0 li");
-    }
-  }
-
-  //delete line if delete button is clicked
-  document.getElementById("del").addEventListener("click", function () {
-    const del = document.querySelector("#del");
-    del.parentNode.remove();
-  });
-});
-
-//remove last li in list
-// document.getElementById("remove-btn").addEventListener("click", function () {
-//   ul.lastElementChild.remove();
-// });
-
-// remove item from the list
-// document.getElementById("delbtn").addEventListener("click", function () {
-//   ul.parentnode.removechild();
-// });
-
-// // remove item using delete button
-// const del = document.getElementById(del);
-// document.getElementById(del).addEventListener("click", function () {
-//   document.getElementById(del).remove;
-// });
+// function to add the new task to the DOM
+document.getElementById("add-btn-id").addEventListener("click", createTask());
