91bea98d2c41708e399c26937c85841dcf3e9257
Rob <36705943+roblatacz@users.noreply.github.com>
2020-05-07 21:58:34 +0100

    Added press enter to add task

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // function to create the task li
-function createTask() {
+function addTask() {
   const li = document.createElement("li");
   const input = document.getElementById("input-id");
   const ul = document.getElementById("ul-id");
@@ -9,7 +9,16 @@ function createTask() {
   console.log(ul);
 }
 
+// function to delete a task
+
 // function to add the new task to the DOM
 document.getElementById("add-btn-id").addEventListener("click", function () {
-  createTask();
+  addTask();
+});
+
+// function to add the new task to the DOM on pressing enter key
+document.getElementById("input-id").addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    document.getElementById("add-btn-id").click();
+  }
 });
