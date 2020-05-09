a28ab26f6a3af9176ef7bb344f5e7ebbf5e4ce3d
Rob <36705943+roblatacz@users.noreply.github.com>
2020-05-09 17:18:48 +0100

    Tasks are now added to a task list array

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
+taskList = [];
+
 // function to create the task li
 function addTask() {
   const li = document.createElement("li");
+  const del = document.createElement("button");
   const input = document.getElementById("input-id");
   const ul = document.getElementById("ul-id");
+  del.setAttribute("class", "del-btn-class");
+  del.setAttribute("id", "del-btn-id");
+  taskList.push(input.value);
+  console.log(taskList);
+  del.innerHTML = "Delete";
+  del.onclick = delTask;
   li.innerHTML = input.value;
+  li.appendChild(del);
   ul.appendChild(li);
   input.value = "";
   console.log(ul);
 }
 
 // function to delete a task
+function delTask(e) {
+  const getButtonID = e.target.id;
+  console.log(getButtonID);
+}
 
-// function to add the new task to the DOM
+// event listener for add task button
 document.getElementById("add-btn-id").addEventListener("click", function () {
   addTask();
 });
